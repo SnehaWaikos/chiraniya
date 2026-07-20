f49a35275585ff38933f5296dc10f46bc28dee5d
SnehaWaikos <snehawaikos2@gmail.com>
2026-07-20 13:52:45 +0530

    update2

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+     return {
+       base: '/chiraniya/',
+       plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
